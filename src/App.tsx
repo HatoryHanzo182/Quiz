465ac252c2a244901eac2bf93c1b3fdcfd88b038
gh0sty <nice140@icloud.com>
2023-05-24 20:22:51 +0300

    !!bf

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,13 @@ import './App.css';
 import Auth from './components/Auth';
 import Quiz from './components/Quiz';
 import Menu from './components/Menu';
+import QuizTest from './components/QuizTest';
 
 function App() 
 {
 
   return (
-    <Auth />
+<QuizTest/>
   );
 }
 
