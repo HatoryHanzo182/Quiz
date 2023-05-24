2110f9989f5ccbe6d33d96c2e551776d990ffb74
gh0sty <nice140@icloud.com>
2023-05-24 12:46:50 +0300

    MenuDesignReady

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,13 @@ import React, { useState } from 'react';
 import './App.css';
 import Auth from './components/Auth';
 import Quiz from './components/Quiz';
+import Menu from './components/Menu';
 
-<<<<<<< HEAD
-
-function App() {
-
-=======
 function App() 
 {
->>>>>>> ac6237947dc757123b877c9323c4ce7e165cce1d
+
   return (
-    <Auth/>
+    <Auth />
   );
 }
 
