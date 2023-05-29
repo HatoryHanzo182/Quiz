4ff47c83c8dbcf36a59eab9dd8508a30e0808d42
HatoryHanzo <scrinnpowerapp@gmail.com>
2023-05-29 19:50:24 +0300

    new functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
-import './App.css';
+import React, { useState } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Auth from './components/Auth';
 import Menu from './components/Menu';
-import QuizTest from './components/QuizTest';
+import TeacherMenu from './components/TeacherMenu';
+import './App.css';
 
 function App() 
 {
+  const [userData, setUserData] = useState(null);  // This hook receives user data and passes it to other components.
+
   return (
-<BrowserRouter>
+    <BrowserRouter>
       <Routes>
-      <Route path="/" element={<Auth />}/>
+      <Route path="/" element={<Auth updateUserData={setUserData} />}/>
+      <Route path="/TeacherMenu" element={userData && <TeacherMenu userData={userData} />} />
       <Route path="/Menu" element={<Menu />} />
       </Routes>
     </BrowserRouter>
