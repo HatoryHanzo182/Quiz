97d31cc87b2564c6bb12087c3b2af41dcf999035
gh0sty <nice140@icloud.com>
2023-06-02 15:36:40 +0300

    fixbugs

--- frontend/src/components/Menu.tsx
+++ frontend/src/components/Menu.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from "react-dom/client";
 import { useNavigate } from 'react-router-dom';
 import Quiz from './Quiz';
+import { VscAccount } from "react-icons/vsc";
 import '../styles/Menu.css';
 
 function Menu() 
@@ -12,23 +13,31 @@ function Menu()
 
   return (
   <>
+
+<nav>
+  <div className="navbar">
+    <a className="logo" href=".">Лого</a>
+   <a className='profile' href='.'><VscAccount size={25}></VscAccount></a>
+   
+
+    </div>
+</nav>
     <div className="ag-format-container">
       <div className="ag-courses_box">
         <div className="ag-courses_item">
           <a className="ag-courses-item_link" onClick={() => handleQuizClick("64775e84880214b8b0d307ad")}>
           <div className="ag-courses-item_bg"></div>
-          <div className="ag-courses-item_title">Software Developer C++</div>
+          <div className="ag-courses-item_title">Basic Quiz C++</div>
           </a>
         </div>
 
 
 
     <div className="ag-courses_item">
-      <a  className="ag-courses-item_link">
+      <a  className="ag-courses-item_link" onClick={()=> handleQuizClick("6479134a880214b8b0757a03")}>
         <div className="ag-courses-item_bg"></div>
-
         <div className="ag-courses-item_title">
-          Software developer JavaScript
+          Basic Quiz HTML
         </div>
 
      
@@ -36,11 +45,11 @@ function Menu()
     </div>
 
     <div className="ag-courses_item">
-      <a className="ag-courses-item_link">
+      <a className="ag-courses-item_link"  onClick={()=> handleQuizClick("6479117c880214b8b073b080")}>
         <div className="ag-courses-item_bg"></div>
 
         <div className="ag-courses-item_title">
-        UI/UX designer
+        Basic Quiz CSS
         </div>
 
        
@@ -52,7 +61,7 @@ function Menu()
         <div className="ag-courses-item_bg"></div>
 
         <div className="ag-courses-item_title">
-          Graphic Design
+         Basic Quiz JavaScript
         </div>
 
        
