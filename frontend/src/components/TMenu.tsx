ca4e1131eea5bc34b62e145e24e811bca484edbc
gh0sty <nice140@icloud.com>
2023-06-03 03:11:49 +0300

    !!!

---
+++ frontend/src/components/TMenu.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import '../styles/TeacherMenu.css';
+
+function TMenu() {
+  return (
+    <>
+  <div className="containerT">
+    <div className="cont">
+
+     
+   </div>
+    <div className="cont2"></div>
+
+
+  </div>
+  
+    </>
+  )
+}
+
+export default TMenu
