346c29d1acc7032b03c1bb0042467f7b2270547b
gh0sty <nice140@icloud.com>
2023-06-05 14:50:19 +0300

    !!!

--- frontend/src/components/TMenu.tsx
+++ frontend/src/components/TMenu.tsx
@@ -1,13 +1,58 @@
 import React from 'react';
 import '../styles/TeacherMenu.css';
-import '../img/test.png'
+import test from '../img/test.png'
 
 function TMenu() {
   return (
     <>
-  <div className="containerT">
-    <div className="cont">
- <div className="card-wrapper">
+
+
+
+<div className="cont">
+  <div className="column">
+    <div className="card">
+    <div className="pic">
+        <img src={test}></img> 
+      </div>
+      <div className="text">
+    <h3>name + surname</h3>
+      <p>Login</p>
+      <p>email</p>
+      </div>
+    </div>
+  </div>
+
+  <div className="column">
+    <div className="card">
+    <div className="pic">
+        <img src={test}></img> 
+      </div>
+      <div className="text">
+    <h3>name + surname</h3>
+      <p>Login</p>
+      <p>email</p>
+      </div>
+    </div>
+  </div>
+
+ 
+</div>
+
+
+{/* <div className="cont">
+<div className="card-wrapper">
+  <div className="card-header">
+    <div className="pic">
+      <img src="./test.png"></img>
+    </div>
+    <h3 className='name'>Alex Black</h3>
+  </div>
+ </div>
+ </div> */}
+ 
+{/* 
+ <div className="cont">
+<div className="card-wrapper">
   <div className="card-header">
     <div className="pic">
       <img src="./test.png"></img>
@@ -16,11 +61,11 @@ function TMenu() {
   </div>
  </div>
 
- </div>
- </div>
+ </div> */}
+
   
     </>
   )
 }
 
-export default TMenu
+export default TMenu;
