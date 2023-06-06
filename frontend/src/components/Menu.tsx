ab5130fc831d8e84d38c27b521b5825c48fc7730
gh0sty <nice140@icloud.com>
2023-06-06 20:33:19 +0300

    !!!

--- frontend/src/components/Menu.tsx
+++ frontend/src/components/Menu.tsx
@@ -25,44 +25,43 @@ function Menu()
         <div className="ag-courses_item">
           <a className="ag-courses-item_link" onClick={() => handleQuizClick("64775e84880214b8b0d307ad")}>
             <div className="ag-courses-item_bg"></div>
-            <div className="ag-courses-item_title">Basic Quiz C++</div>
+            <div className="ag-courses-item_title">Quiz C++</div>
           </a>
         </div>
     <div className="ag-courses_item">
       <a  className="ag-courses-item_link" onClick={()=> handleQuizClick("6479134a880214b8b0757a03")}>
         <div className="ag-courses-item_bg"></div>
-        <div className="ag-courses-item_title">
-          Basic Quiz HTML</div>
+        <div className="ag-courses-item_title">Quiz HTML</div>
       </a>
     </div>
     <div className="ag-courses_item">
       <a className="ag-courses-item_link"  onClick={()=> handleQuizClick("6479117c880214b8b073b080")}>
         <div className="ag-courses-item_bg"></div>
-        <div className="ag-courses-item_title">Basic Quiz CSS</div>
+        <div className="ag-courses-item_title">Quiz CSS</div>
       </a>
     </div>
     <div className="ag-courses_item">
       <a  className="ag-courses-item_link">
         <div className="ag-courses-item_bg"></div>
-        <div className="ag-courses-item_title">Basic Quiz JavaScript</div>
+        <div className="ag-courses-item_title">Quiz JavaScript</div>
       </a>
     </div>
     <div className="ag-courses_item">
-      <a  className="ag-courses-item_link">
+    <a className="ag-courses-item_link"  onClick={()=> handleQuizClick("647f6c67880214b8b03c837a")}>
         <div className="ag-courses-item_bg"></div>
-        <div className="ag-courses-item_title">Motion Design</div>
+        <div className="ag-courses-item_title">Quiz TypeScript</div>
       </a>
     </div>
     <div className="ag-courses_item">
-      <a  className="ag-courses-item_link">
+    <a className="ag-courses-item_link"  onClick={()=> handleQuizClick("647f6786880214b8b037ea05")}>
         <div className="ag-courses-item_bg"></div>
-        <div className="ag-courses-item_title">Front-end development</div>
+        <div className="ag-courses-item_title">Quiz Python</div>
       </a>
     </div>
     <div className="ag-courses_item">
-      <a  className="ag-courses-item_link">
+    <a className="ag-courses-item_link"  onClick={()=> handleQuizClick("647f6234880214b8b032f7ed")}>
         <div className="ag-courses-item_bg"></div>
-        <div className="ag-courses-item_title">Interior Design</div>
+        <div className="ag-courses-item_title">Quiz SQL</div>
       </a>
     </div>
     </div>
