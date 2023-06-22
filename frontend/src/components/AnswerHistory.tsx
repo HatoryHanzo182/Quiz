7f7221b7ea22210c383d2fe188d9c3d3a254292f
HatoryHanzo <scrinnpowerapp@gmail.com>
2023-06-22 17:17:44 +0300

    !!

--- frontend/src/components/AnswerHistory.tsx
+++ frontend/src/components/AnswerHistory.tsx
@@ -5,54 +5,59 @@ import style from '../styles/AnswerHistory.module.css';
 
 function AnswerHistory()
 {
-    const [userData, setUserData] = useState<User>();  // Hook to get user data. 
-    const [answerHistory, setAnswerHistory] = useState<{ question: string; userAnswer: string | undefined; correctAnswer: string | undefined; }[]>([]);
+  /*
+      * The AnswerHistory component displays the user's response history.
+      * It uses state to get user data and response history.
+      * If there is no user data, the user will be redirected to the main page.
+      * Answer history is displayed as a list with questions, user data and correct answers.
+      * The component also has responsive styles to look good on different devices.
+  */
+  const [userData, setUserData] = useState<User>();  // Hook to get user data. 
+  const [answerHistory, setAnswerHistory] = useState<{ question: string; userAnswer: string | undefined; correctAnswer: string | undefined; }[]>([]);
 
-    const navigate = useNavigate();  // Navigation to transfer to the menu.
+  const navigate = useNavigate();  // Navigation to transfer to the menu.
   
-    useEffect(() =>  // The hook runs after the page is loaded.
+  useEffect(() =>  // The hook runs after the page is loaded.
+  {
+    const ariownUserData = async () =>  // Get user data from browser local storage.
     {
-        const ariownUserData = async () =>  // Get user data from browser local storage.
-        {
-          const storedUserData = sessionStorage.getItem('userData');
+      const storedUserData = sessionStorage.getItem('userData');
         
-          if (storedUserData) 
-            setUserData(JSON.parse(storedUserData));
-          else
-            navigate('/')
-        }
-
-        function pullHistory()
-        {
-            const storedHistory = sessionStorage.getItem('ansHistory');
-            
-            if (storedHistory) 
-                setAnswerHistory(JSON.parse(storedHistory));
-        }
-
-        ariownUserData();
-        pullHistory();
-    }, [])
-
-    return (
-    <>
-      {/*Response story content.*/}
-      <div className={style.card}>
-        <h1>Answers</h1>
-        <div className={style['card-content']}>{answerHistory.map((answer, index) => (
-          <div key={index}>
-            <hr />
-            <br />
-            <p>{answer.question}</p>
-            <p className={answer.userAnswer === answer.correctAnswer ? style.correct : style.incorrect}>
-              Your answer: {answer.userAnswer}
-            </p>
-            <p className={style.correct}>Correct answer: {answer.correctAnswer}</p>
-            <br />
-          </div>))}
-        </div>
+      if (storedUserData) 
+        setUserData(JSON.parse(storedUserData));
+      else
+        navigate('/')
+    }
+    
+    function pullHistory()
+    {
+      const storedHistory = sessionStorage.getItem('ansHistory');
+      
+      if (storedHistory) 
+        setAnswerHistory(JSON.parse(storedHistory));
+    }
+    
+    ariownUserData();
+    pullHistory();
+  }, [])
+  
+  return (
+  <>
+    {/*Response story content.*/}
+    <div className={style.card}>
+      <h1>Answers</h1>
+      <div className={style['card-content']}>{answerHistory.map((answer, index) => (
+        <div key={index}>
+          <hr />
+          <br />
+          <p>{answer.question}</p>
+          <p className={answer.userAnswer === answer.correctAnswer ? style.correct : style.incorrect}>Your answer: {answer.userAnswer}</p>
+          <p className={style.correct}>Correct answer: {answer.correctAnswer}</p>
+          <br />
+        </div>))}
       </div>
-    </>)
+    </div>
+  </>)
 }
 
 export default AnswerHistory;
--- frontend/src/components/TeacherMenu.tsx
+++ frontend/src/components/TeacherMenu.tsx
@@ -1,107 +1,115 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { VscAccount } from "react-icons/vsc";
+import axios from 'axios';
 import User from '../modules/UserDataModel';
 import Result from '../modules/ResultModel';
-import axios from 'axios';
-import '../styles/TeacherMenu.css';
+import style from '../styles/TeacherMenu.module.css';
 
-function TeacherMenu()
+function TeacherMenu() 
 {
-    const [userData, setUserData] = useState<User>();  // Hook to get user data.
-    const [resultsQuizzes, setResultsQuizzes] = useState<Result[]>([]);
-    const [selectedStudent, setSelectedStudent] = useState<Result | null>(null);
+  const [userData, setUserData] = useState<User>();
+  const [resultsQuizzes, setResultsQuizzes] = useState<Result[]>([]);
+  const [selectedStudent, setSelectedStudent] = useState<Result | null>(null);
 
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    useEffect(() =>  // Hook for processing data when the page is fully loaded.
+  useEffect(() => 
+  {
+    const fetchUserData = async () => 
     {
-        const ariownUserData = async () =>  // Get teacher data from browser local storage.
-        {
-            const storedUserData = sessionStorage.getItem('userData');
-
-            if (storedUserData) 
-                setUserData(JSON.parse(storedUserData));
-            else
-                navigate('/')
-        }
-        
-        const fetchStudentData = async () => 
-        {
-            try 
-            {
-                const response = await axios.get('http://localhost:3000/results');
-                
-                setResultsQuizzes(response.data);
-            } 
-            catch (error) { console.error('Error while fetching student data:', error); }
-        };
-        
-        ariownUserData();
-        fetchStudentData();
-    }, []);
-
-    const handleProfileClick = (event: any) =>  // Profile transition event. 
-    {
-      event.preventDefault(); 
-  
-      navigate(`/TeacherMenu/Profile`);
-    };
-
-    const handleRowClick = (student: any) => 
-    {
-      setSelectedStudent(student);
-    
-      if (student.verified === "false") 
-      {
-        const updatedStudent = { ...student, verified: "true" };
-        const updatedResults = resultsQuizzes.map((s) => s._id === student._id ? updatedStudent : s);
-    
-        setResultsQuizzes(updatedResults);
-
-        console.log(student._id);
-
-        axios.put(`http://localhost:3000/results/${student._id}`, { verified: "true" }).then(() => 
-        {
-          const updatedResultsLocal = resultsQuizzes.map((s) => s._id === student._id ? updatedStudent : s);
-
-          setResultsQuizzes(updatedResultsLocal);
-        }).catch((error) => { console.error("Error updating student verification:", error); });
+      const storedUserData = sessionStorage.getItem('userData');
+      if (storedUserData) {
+        setUserData(JSON.parse(storedUserData));
+      } else {
+        navigate('/');
       }
     };
 
-    if (!resultsQuizzes || resultsQuizzes.length === 0)
+    const fetchResultsQuizzes = async () => {
+      try {
+        const response = await axios.get('http://localhost:3000/results');
+        setResultsQuizzes(response.data);
+      } catch (error) {
+        console.error('Error while fetching student data:', error);
+      }
+    };
+
+    fetchUserData();
+    fetchResultsQuizzes();
+  }, []);
+
+  const handleProfileClick = (event: any) => 
+  {
+    event.preventDefault();
+    
+    navigate('/TeacherMenu/Profile');
+  };
+
+  const handleRowClick = (student: Result) =>  // Sets the selected student to the selectedStudent state using the setSelectedStudent(student) function.
+  {                                           // setSelectedStudent(student) function.
+                                             // Checks the value of the verified property of the student. If it is equal to the string "false", then additional actions are performed.
+                                            // A copy of the selected student is created with the verified property updated to "true". The updated student is stored in the updatedStudent variable.
+    setSelectedStudent(student);
+
+    if (student.verified === "false") 
     {
-      return (
+      const updatedStudent = { ...student, verified: "true" };
+      const updatedResults = resultsQuizzes.map((s) => (s._id === student._id ? updatedStudent : s));
+
+      setResultsQuizzes(updatedResults);
+
+      axios.put(`http://localhost:3000/results/${student._id}`, { verified: "true" }).then(() => 
+      {
+        const updatedResultsLocal = resultsQuizzes.map((s) => (s._id === student._id ? updatedStudent : s));
+        
+        setResultsQuizzes(updatedResultsLocal);
+      }).catch((error) => { console.error("Error updating student verification:", error);});
+    }
+  };
+
+  const filteredResults = resultsQuizzes.filter((result) => result.teacher_code === (userData && userData.teacher_code));  // Filter results based on teacher code.
+
+  if (!filteredResults || filteredResults.length == 0)  // Displayed if there are still no results.
+  {
+    return (
       <>
+        {/*Content in the absence of data.*/}
         <div>
-          <div className="navbar">
-            <a className="profile" href="." onClick={(event) => handleProfileClick(event)}><VscAccount size={25} /></a>
+          <div className={style.navbar}>
+            {/*Profile exit icon.*/}
+            <a className={style.profile} href="." onClick={handleProfileClick}>
+              <VscAccount size={25} />
+            </a>
           </div>
-          <div className="mycent">
-            <div className="container1">
-              <div className="content">
+          {/*Main content.*/}
+          <div className={style.mycent}>
+            <div className={style.container1}>
+              <div className={style.content}>
                 <h1 className={userData && userData.teacher_code.toString()}>Teacher code: {userData && userData.teacher_code}</h1>
                 <h2>You don't have new results</h2>
               </div>
             </div>
           </div>
         </div>
-      </>)
-    }
+      </>
+    );
+  }
 
-    return(
-      <>
-      <div>
-        <div className="navbar">
-          <a className="profile" href="." onClick={(event) => handleProfileClick(event)}>
-            <VscAccount size={25} />
-          </a>
-        </div>
-        <div className="mycent">
-          <div className="container1">
-            <div className="content">
-              <h1 className={userData && userData.teacher_code.toString()}>Teacher code: {userData && userData.teacher_code}</h1>
+
+  return (
+  <>
+    <div className={style.container}>
+      <div className={style.navbar}>
+        <a className={style.profile} href="." onClick={handleProfileClick}>
+          <VscAccount size={25} />
+        </a>
+      </div>
+      <div className={style.mycent}>
+        <div className={style.container1}>
+          <div className={style.content}>
+            <h1 className={userData && userData.teacher_code.toString()}>Teacher code: {userData && userData.teacher_code}</h1>
+            <div className={style.tableWrapper}>
               <table>
                 <thead>
                   <tr>
@@ -113,10 +121,10 @@ function TeacherMenu()
                   </tr>
                 </thead>
                 <tbody>
-                  {resultsQuizzes.slice().reverse().map((student) => (
+                  {filteredResults.slice().reverse().map((student) => (
                     <tr key={student._id.toString()} onClick={() => handleRowClick(student)}>
-                      <td className="student-cell">
-                        {student.verified === "false" && <span className="new-result-label">New</span>}
+                      <td className={style['student-cell']}>
+                        {student.verified === "false" && <span className={style['new-result-label']}>New</span>}
                         {student.student}
                       </td>
                       <td>{student.quiz_title}</td>
@@ -128,27 +136,33 @@ function TeacherMenu()
                 </tbody>
               </table>
             </div>
-            <div className="quiz-history">
-              {selectedStudent && (
-                <div>
-                  <h2>{selectedStudent.quiz_title} for {selectedStudent.student}</h2>
-                  {selectedStudent.answer_history && selectedStudent.answer_history.map((answer, index) => (
-                    <div key={index}>
-                      <hr />
-                      <br />
-                      <p>{answer.question}</p>
-                      <p className={answer.userAnswer === answer.correctAnswer ? "correct" : "incorrect"}>Answer: {answer.userAnswer}</p>
-                      <p className='correct'>Correct Answer: {answer.correctAnswer}</p>
-                      <br/>
-                    </div>
-                  ))}
-                </div>
-              )}
-            </div>
           </div>
         </div>
+        <div className={style['quiz-history-panel']}>
+          {selectedStudent ? (
+            <div className={style['quiz-history']}>
+              <h2>{selectedStudent.quiz_title} for {selectedStudent.student}</h2>
+              {selectedStudent.answer_history && selectedStudent.answer_history.map((answer, index) => (
+                <div key={index}>
+                  <hr />
+                  <br />
+                  <p>{answer.question}</p>
+                  <p className={answer.userAnswer === answer.correctAnswer ? style.correct : style.incorrect}>Answer: {answer.userAnswer}</p>
+                  <p className={style.correct}>Correct Answer: {answer.correctAnswer}</p>
+                  <br />
+                </div>
+              ))}
+            </div>
+          ) : (
+            <div className={style['centered-text']}>
+              <h3>History of answers to quizzes</h3>
+              <p>To see the history, click on the student in the table on the right.</p>
+            </div>
+          )}
+        </div>
       </div>
-    </>)
-} 
+    </div>
+  </>);
+}
 
 export default TeacherMenu;
